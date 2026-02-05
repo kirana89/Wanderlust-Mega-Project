9edba1b0bde49b09431e4fdadbf428c0bb9e1bf2
kirana89 <kiran897020@gmail.com>
2026-02-05 13:02:20 +0000

    added inew instance ids to automation/updatebackend nad frontend

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-0bd573c00d82e4e3f"
+INSTANCE_ID="i-07210c9079bd192f6"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
